8f3b333f3c3deb6b8d4d5b0d36fef90d6c1d04af
Kai McGregor <45206927+kmcgreg5@users.noreply.github.com>
2024-02-02 20:51:32 -0800

    Add record command and API added

---
+++ technitiumAPI.py
@@ -0,0 +1,94 @@
+from requests import Session, Response
+import ipaddress
+
+class APIException(Exception):
+    pass
+
+class TechnitiumAPI:
+    def __init__(self, host: str=None, username: str=None, password: str=None):
+        self._host = host
+        self._session = None
+        self.__token = None
+        self.__username = username
+        self.__password = password
+        
+
+    def get_zones(self) -> list:
+        endPoint = f'/api/zones/list'
+        response = self._session.post(self.__get_url(endPoint))
+        return self.__validate_response(response, 'response', 'zones')
+
+    def get_records(self, zone: str):
+        endPoint = f'/api/zones/records/get'
+        response = self._session.post(self.__get_url(endPoint, domain=zone, listZone='true'))
+        return self.__validate_response(response, 'response', 'records')
+
+    def add_record(self, zone: str, subdomain: str, ip: str):
+        ipaddress.ip_address(ip)
+        endPoint = f'/api/zones/records/add'
+
+        response = self._session.post(self.__get_url(endPoint, domain=f'{subdomain}.{zone}', zone=zone, type='A', ipAddress=ip))
+        self.__validate_response(response)
+
+    def __validate_response(self, response, *args):
+        if response.status_code != 200:
+            raise APIException(response.text)
+        
+        json = response.json()
+        if json['status'] != 'ok':
+            raise APIException(json)
+        
+        if len(args) == 0:
+            return
+        
+        return_value = json
+        for arg in args:
+            return_value = return_value[arg]
+        
+        return return_value
+
+
+    def __get_host(self):
+        return f'http://{self._host}:5380'
+
+    def __get_url(self, endPoint, **arguments) -> str:
+        url = f'{self.__get_host()}{endPoint}?token={self.__token}'
+        for key, value in arguments.items():
+            url = f'{url}&{key}={value}'
+        
+        return url
+
+
+    def set_target_info(self, host: str, username: str, password: str):
+        self._host = host
+        self.__username = username
+        self.__password = password
+
+    def start_session(self):
+        if self._session is None:
+            self._session = Session()
+            self.__token = self.__get_token()
+
+    def end_session(self):
+        if self._session is not None:
+            self.__logout()
+            self._session.close()
+            self._session = None
+    
+    def __logout(self):
+        endPoint = f'/api/user/logout'
+        self._session.post(self.__get_url(endPoint))
+
+    def __get_token(self) -> str:
+        url = f'{self.__get_host()}/api/user/login?user={self.__username}&pass={self.__password}&includeInfo=false'
+        self.__password = None
+
+        response = self._session.post(url)
+        return self.__validate_response(response, 'token')
+    
+    def __enter__(self):
+        self.start_session()
+        return self
+    
+    def __exit__(self, *args):
+        self.end_session()
---
+++ technitiumCLI.py
@@ -0,0 +1,87 @@
+from technitiumAPI import TechnitiumAPI
+from argparse import ArgumentParser
+import sys
+
+class CLIException(Exception):
+    pass
+
+def main(cliArgs: list=sys.argv[1:]):
+    # Main parser
+    parser = ArgumentParser(prog="Technitium DNS Server CLI")
+    parser.add_argument("--host", help="The Technitium DNS Server host.")
+    parser.add_argument("--username", help="The Technitium DNS Server host.", nargs='?')
+    parser.add_argument("--password", help="The Technitium DNS Server host.", nargs='?')
+    parser.add_argument("--port", help="The port to connect to.", nargs='?', type=int)
+
+    items = parser.add_subparsers(help="The supported items to operate on.", dest="item")
+
+    # Record parser
+    record_parser = items.add_parser("record")
+    operations = record_parser.add_subparsers(help="The operation to perform", dest="operation")
+    # Add Record parser
+    record_add_parser = operations.add_parser("add")
+    record_add_parser.add_argument("zone", help="The zone to add to.")
+    record_add_parser.add_argument("subdomain", help="The subdomain to add.")
+    record_add_parser.add_argument("ip", help="The ip the record points to.")
+
+    args = parser.parse_args(cliArgs)
+
+    if args.item == 'record':
+        if args.operation == 'add':
+            __validate_options(args)
+            __add_record(args)
+        else:
+            parser.print_help()
+            sys.exit(1)
+    else:
+        parser.print_help()
+        sys.exit(1)
+    
+    print("Success")
+
+def __validate_options(args):
+    def throwRequiredOptionException(option: str):
+        parsed_option = option
+        while parsed_option.startswith('-'):
+            parsed_option = parsed_option[1:]
+
+        value = getattr(args, parsed_option)
+        if value is None:
+            raise CLIException(f'The option \'{option}\' is undefined.')
+
+    throwRequiredOptionException("--host")
+    throwRequiredOptionException("--username")
+    throwRequiredOptionException("--password")
+    throwRequiredOptionException("--port")
+
+def __add_record(args):
+    with TechnitiumAPI(args.host, args.username, args.password) as server:
+        zones = server.get_zones()
+        zoneFound = False
+        for zone in zones:
+            if zone['name'] == args.zone:
+                zoneFound = True
+                break
+        
+        if zoneFound is False:
+            raise CLIException(f'The zone \'{args.zone}\' could not be found.')
+        
+        records = server.get_records(args.zone)
+        recordExists = False
+        for record in records:
+            if record['name'] == f'{args.subdomain}.{args.zone}':
+                recordExists = True
+                break
+        
+        if recordExists is True:
+            raise CLIException(f'The record for \'{args.subdomain}.{args.zone}\' already exists.')
+        
+        server.add_record(args.zone, args.subdomain, args.ip)
+
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception as e:
+        print(f'Exception occurred: {str(e)}')
+        sys.exit(1)
+    
